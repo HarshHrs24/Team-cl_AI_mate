32e05695bb64d6a519f92d5efaa19264318165ea
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-02-26 11:52:29 +0530

    PUSH HRS

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ with st.container():
     cities = gpd.read_file("cities.geojson")
 
     # Create a folium map centered on the India
-    m = folium.Map(location=[17.9774221, 79.52881], zoom_start=7)
+    m = folium.Map(location=[17.9774221, 79.52881], zoom_start=6)
 
     # Create a GeoJson layer for the city data
     geojson = folium.GeoJson(

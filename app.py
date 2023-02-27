e0ca12c798f252851ec75d12ba8f158755947ce4
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-02-28 03:03:48 +0530

    push shiv

--- app.py
+++ app.py
@@ -186,6 +186,39 @@ with st.container():
     st.header("Graph")
     fig1 = plot_plotly(m, forecast)
     st.plotly_chart(fig1)
+    
+# Heat wave timeline
+st.header(" Major Heat wave occurrences in the year 2023")
+items = [
+    {"id": 1, "content": "2023-01-20", "start": "2023-03-01"},
+    {"id": 2, "content": "2023-10-09", "start": "2023-04-09"},
+    {"id": 3, "content": "2023-10-18", "start": "2023-05-18"},
+    {"id": 4, "content": "2023-10-16", "start": "2023-06-16"},
+    {"id": 5, "content": "2023-10-25", "start": "2023-07-25"},
+    {"id": 6, "content": "2023-10-27", "start": "2023-08-27"},
+]
+
+timeline = st_timeline(items, groups=[], options={}, height="300px")
+# st.subheader("Selected item")
+# st.write(timeline)
+
+# Define start and end dates
+start_date = datetime.date(2023, 1, 1)
+end_date = datetime.date(2023, 12, 31)
+
+
+# Create date input
+selected_date = st.date_input(
+    "Choose a date for the year 2023",
+    value=datetime.date(2023, 1, 1),
+    min_value=start_date,
+    max_value=end_date,
+    key="date_input"
+)
+
+# Display selected date
+st.write("You selected:", selected_date.strftime("%B %d, %Y"))
+
 
 st.write("---")
 st.subheader("Choose a date")

70279afbcc0873dbd3696201800d451ea94a4b2a
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-02-27 13:18:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -269,9 +269,9 @@ with st.container():
 
     left_column, middle_column, right_column = st.columns(3)
     with left_column:
-        st.write("<p style='color: #FF1493; font-size: 20px;'>Temperature(°F) : {}</p>".format(df.loc[d, 'temp']), unsafe_allow_html=True)
-        st.write("<p style='color: #FF1493; font-size: 20px;'>Humidity : {}</p>".format(df.loc[d, 'humidity']), unsafe_allow_html=True)
-        st.write("<p style='color: #FF1493; font-size: 20px;'>Preciptation : {}</p>".format(df.loc[d, 'precip']), unsafe_allow_html=True)
+        st.write("<p style='color: #00C957; font-size: 20px;'>Temperature(°F) : {}</p>".format(df.loc[d, 'temp']), unsafe_allow_html=True)
+        st.write("<p style='color: #00C78C; font-size: 20px;'>Humidity : {}</p>".format(df.loc[d, 'humidity']), unsafe_allow_html=True)
+        st.write("<p style='color: #008B45 font-size: 20px;'>Preciptation : {}</p>".format(df.loc[d, 'precip']), unsafe_allow_html=True)
         st.write("<p style='color: #FF1493; font-size: 20px;'>Wind speed : {}</p>".format(df.loc[d, 'windspeed']), unsafe_allow_html=True)
     with right_column:
         st.write("<p style='color: #FF1493; font-size: 20px;'>Cloud cover : {}</p>".format(df.loc[d, 'cloudcover']), unsafe_allow_html=True)

54f93b5d3db808fc533b2511c5ec1607c017487d
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-02-28 03:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import smtplib
 import datetime
 from shapely.geometry import Point
 import base64
-from streamlit_timeline import timeline
+# from streamlit_timeline import st_timeline
 
 
 
@@ -190,39 +190,40 @@ with st.container():
     
 # Heat wave timeline
 st.header(" Major Heat wave occurrences in the year 2023")
-items = [
-    {"id": 1, "content": "2023-01-20", "start": "2023-03-01"},
-    {"id": 2, "content": "2023-10-09", "start": "2023-04-09"},
-    {"id": 3, "content": "2023-10-18", "start": "2023-05-18"},
-    {"id": 4, "content": "2023-10-16", "start": "2023-06-16"},
-    {"id": 5, "content": "2023-10-25", "start": "2023-07-25"},
-    {"id": 6, "content": "2023-10-27", "start": "2023-08-27"},
-]
-
-gto_timeline = timeline(items)
-st.subheader("Selected item")
-st.write(gto_timeline)
-
-# events = [
-#     {
-#         "start": "2022-01-01",
-#         "title": "Event 1",
-#         "description": "This is event 1"
-#     },
-#     {
-#         "start": "2022-02-01",
-#         "title": "Event 2",
-#         "description": "This is event 2"
-#     },
-#     {
-#         "start": "2022-03-01",
-#         "title": "Event 3",
-#         "description": "This is event 3"
-#     }
+# items = [
+#     {"id": 1, "content": "2023-01-20", "start": "2023-03-01"},
+#     {"id": 2, "content": "2023-10-09", "start": "2023-04-09"},
+#     {"id": 3, "content": "2023-10-18", "start": "2023-05-18"},
+#     {"id": 4, "content": "2023-10-16", "start": "2023-06-16"},
+#     {"id": 5, "content": "2023-10-25", "start": "2023-07-25"},
+#     {"id": 6, "content": "2023-10-27", "start": "2023-08-27"},
 # ]
 
-# # Display the timeline
-# timeline(events)
+# timeline = st_timeline(items, groups=[], options={}, height="300px")
+# st.subheader("Selected item")
+# st.write(timeline)
+events = [
+    {
+        "start": "2022-01-01",
+        "title": "Event 1",
+        "description": "This is event 1"
+    },
+    {
+        "start": "2022-02-01",
+        "title": "Event 2",
+        "description": "This is event 2"
+    },
+    {
+        "start": "2022-03-01",
+        "title": "Event 3",
+        "description": "This is event 3"
+    }
+]
+if selected_event:
+    st.write(f"You selected the event '{selected_event['title']}'")
+
+
+selected_event = st_timeline(events, height=500, start_zoom=1, end_zoom=5)
 
 # Define start and end dates
 start_date = datetime.date(2023, 1, 1)

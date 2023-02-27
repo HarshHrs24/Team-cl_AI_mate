4584e1eda59732c50dce6b7c2501fe5aabe0a80d
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-02-27 14:25:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,8 @@ def embed_pdf(pdf_file):
     with fitz.open(pdf_file) as doc:
         page = doc.load_page(0)  # Load the first page of the PDF
         pix = page.get_pixmap()
-        img = pix.to_image()
+        img_bytes = pix.tobytes()
+        img = Image.frombytes("RGB", [pix.width, pix.height], img_bytes)
         st.sidebar.image(img, caption=pdf_file, use_column_width=True)
 
 pdf_file = "json/Solution Architecture(Team cl_AI_mate).pdf"

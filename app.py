25832c84714d2b5317d18ddabb36858878f93681
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-02-28 23:17:46 +0530

    push shiv

--- app.py
+++ app.py
@@ -573,63 +573,58 @@ with st.container():
 
 # ---- CONTACT ----
 with st.container():
-        st.write("---")
-        st.header("Get In Touch With Us")
-        st.write("##")
+    st.write("---")
+    st.header("Get In Touch With Us")
+    st.write("##")
+
+ 
+    def send_email(name, email, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("teamclAImate2023@gmail.com", "balzsvjxdmgtsuts")
+        msg = f"Subject: New message from {name}\n\n{name} ({email}) sent the following message:\n\n{message}"
+        server.sendmail("teamclAImate2023@gmail.com", "teamclAImate2023@gmail.com", msg)
+        st.success("Thank you for contacting us.")
+        
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    message = st.text_area("Message")
+
+    if st.button("Send"):
+        send_email(name, email, message)
 
     
-        def send_email(name, email, message):
-                server = smtplib.SMTP("smtp.gmail.com", 587)
-                server.starttls()
-                server.login("teamclAImate2023@gmail.com", "balzsvjxdmgtsuts")
-                msg = f"Subject: New message from {name}\n\n{name} ({email}) sent the following message:\n\n{message}"
-                server.sendmail("teamclAImate2023@gmail.com", "teamclAImate2023@gmail.com", msg)
-                st.success("Thank you for contacting us.")
-                    
-                name = st.text_input("Name")
-                email = st.text_input("Email")
-                message = st.text_area("Message")
-
-                if st.button("Send"):
-                    send_email(name, email, message)
-
-                
-                st.markdown(
-                """
-                <style>
-                
-
-                    /* Adjust the width of the form elements */
-                    .stTextInput {
-                        width: 50%;
-
-                    }
-                    
-                    .stTextArea {
-                        width: 20%;
-                    }
-
-                    /* Style the submit button */
-                    .stButton button {
-                        background-color: #45a049;
-                        color: #FFFFFF;
-                        font-weight: bold;
-                        padding: 10px;
-                        border-radius: 5px;
-                        width: 10%;
-                    }
-
-                    /* Style the success message */
-                    .stSuccess {
-                        color: #0072C6;
-                        font-weight: bold;
-                        margin-top: 20px;
-                    }
-                </style>
-                """,
-                unsafe_allow_html=True
-            )
-
+    st.markdown(
+    """
+    <style>
+       
+         /* Adjust the width of the form elements */
+        .stTextInput {
+            width: 50%;
+        }
+        
+        .stTextArea {
+            width: 20%;
+        }
+        /* Style the submit button */
+        .stButton button {
+            background-color: #45a049;
+            color: #FFFFFF;
+            font-weight: bold;
+            padding: 10px;
+            border-radius: 5px;
+            width: 10%;
+        }
+        /* Style the success message */
+        .stSuccess {
+            color: #0072C6;
+            font-weight: bold;
+            margin-top: 20px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 

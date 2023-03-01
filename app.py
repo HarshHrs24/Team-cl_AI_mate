3034d7f4414afcaa4a4c6c0693429d5f10315f23
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-03-02 01:06:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -341,9 +341,9 @@ with st.container():
                 )
 
     else:
-         c1, c2, c3, c4, c4, c5 = st.columns(7)
+         c1, c2, c3, c4, c5, c6, c7 = st.columns(7)
 
-         with c2:
+         with c3:
             """### Pollution trend over the decade"""
             gif1="images/AQI/{}_poln.gif".format(selected_city)
             file_ = open(gif1, "rb")

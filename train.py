0e38677cb3a0e1928e4dfc1c2fe9b5bc4071cfac
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-03-02 01:36:02 +0530

    push

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import seaborn as sns
-
+import requests
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,11 +10,21 @@ import shutil
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from prophet.serialize import model_to_json, model_from_json
 import math
-
+import time
 import pandas as pd
-
 from prophet import *
 
+def cord(city):
+
+# Initialize Nominatim API
+   dir = {
+    'Adilabad':(19.6625054 ,78.4953182),
+    'Nizamabad':(18.6804717 ,78.0606503),
+    'Karimnagar':(18.4348833 ,79.0981286),
+    'Khammam':(17.2484683 ,80.006904),
+    'Warangal':(17.9774221 ,79.52881)
+    }
+   return dir[city]
   
 def prophet_train(train ):
   m = Prophet(
@@ -53,10 +63,86 @@ def train_m(train):
   m = Prophet()
   m.fit(train)
   return m
+def weekly_update(city,model):
 
+    api_key = "9ce9f54d7dd8da5535e273da3e9f1a72"
+    lat, lon = cord(city)
+    retrain_log_path="retrain/{}/{}_retrain_log.csv".format(model,city)
+    df = pd.read_csv(retrain_log_path)
+    new_value1 = int(time.time())
+    print(new_value1)
+    start = df['last updated date'].iloc[-1] + 60 * 60
+    end=df['last updated date'].iloc[-1]
+    print(df['last updated date'].iloc[-1]+ 7*24*60*60)
+    while(new_value1>end):
+      end=end+ 7*24*60*60
+
+    end=end- 7*24*60*60
+    new_value2 = end
+    # df['last updated date'] = df['last updated date'].append(pd.Series([new_value2]))
+    # df['retrain datetime'] = df['retrain datetime'].append(pd.Series([new_value1]))
+    df = df.append({'last updated date': new_value2 , 'retrain datetime' :new_value1}, ignore_index=True)
+    # Save the updated DataFrame to the CSV file
+    df.to_csv(retrain_log_path, index=False)
+
+
+    # Construct the API URL with the given parameters
+    if model=='AQI':
+          url = f"http://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={lon}&type=hour&start={start}&end={end}&appid={api_key}"
+          response = requests.get(url)
+
+          # If the response was successful (status code 200), then print the JSON data
+
+
+          data = response.json()
+          w = pd.DataFrame()
+          # for i in range(10):
+          for i in data['list']:
+              # df = df.append({'A': i , 'B' :i, 'C' :i, 'D' :i, 'E' :i, 'F' :i, 'G' :i, 'H' :i}, ignore_index=True)
+              w = w.append({'aqi': i['main']['aqi'] ,
+              'CO' :i['components']['co'],
+                'no2' :i['components']['no2'],
+                'o3' :i['components']['o3'],
+                  'so2' :i['components']['so2'],
+                  'pm2_5' :i['components']['pm2_5'],
+                    'nh3' :i['components']['nh3'],
+                    'dt' :dt.datetime.fromtimestamp(i['dt'])},
+                      ignore_index=True)
+              
+          df_new = pd.read_csv('1_Adilabad_data.csv')
+          k=pd.concat([df_new, w])
+          # k.to_csv('1_Adilabad_data.csv', index=False)
+                      
+
+
+
+    else: 
+          w = pd.DataFrame()
+          while(end>=start):
+              url=f"http://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={start}&appid=9ce9f54d7dd8da5535e273da3e9f1a72"
+              response = requests.get(url)
+              data = response.json()
+              # for i in range(10):
+              for d in data['data']:
+                  # Extract the values from each dictionary and append to the DataFrame
+                  w = w.append({
+                      'datetime': dt.datetime.fromtimestamp(d['dt']),
+                      'temp': d['temp'],
+                      'humidity': d['humidity'],
+                      'windspeed': d['wind_speed'],
+                      'cloudcover': d['clouds'],
+                      'conditions': d['weather'][0]['description'],
+                  }, ignore_index=True)
+              start=start+60*60
+          # w=w[['datetime','temp','humidity','windspeed','cloudcover','conditions']]
+          df_new = pd.read_csv('1_Adilabad_data.csv')
+          df_new['datetime'] =  pd.to_datetime(df_new['datetime'])
+          k=pd.concat([df_new, w])
+          # k.to_csv('1_Adilabad_data.csv', index=False)
+    return k
 def heatwave_train_model(city):
   #declaration 
-  CSV="content/Heat wave/{}.csv".format(city)
+  # CSV="content/Heat wave/{}.csv".format(city)
   one_prediction_model_name="versioning/one/Heat wave/1_{}_model.json".format(city)
   one_prediction_file_name="versioning/one/Heat wave/1_{}_prediction.csv".format(city)
 
@@ -72,6 +158,8 @@ def heatwave_train_model(city):
   winner_prediction_model_name="winner/Heat wave/winner_{}_model.json".format(city)
   winner_prediction_file_name="winner/Heat wave/winner_{}_prediction.csv".format(city)
 
+
+
   #preprocessing   
   
   os.rename(three_prediction_model_name, four_prediction_model_name)
@@ -82,8 +170,10 @@ def heatwave_train_model(city):
 
   os.rename(one_prediction_model_name, two_prediction_model_name)
   os.rename(one_prediction_file_name, two_prediction_file_name)
+  #weekly update via api
 
-  df = pd.read_csv(CSV)
+  # df = pd.read_csv(CSV)
+  df = weekly_update(city,'Heat wave')
   print(df.head())
   
   df['datetime'] =  pd.to_datetime(df['datetime'], format='%Y%m%d %H:%M:%S')
@@ -165,7 +255,7 @@ heatwave_train_model('Warangal')
 
 def aqi_train_model(city):
   #declaration 
-  CSV="content/AQI/{}.csv".format(city)
+  # CSV="content/AQI/{}.csv".format(city)
   one_prediction_model_name="versioning/one/AQI/1_{}_model.json".format(city)
   one_prediction_file_name="versioning/one/AQI/1_{}_prediction.csv".format(city)
 
@@ -192,7 +282,7 @@ def aqi_train_model(city):
   os.rename(one_prediction_model_name, two_prediction_model_name)
   os.rename(one_prediction_file_name, two_prediction_file_name)
 
-  df = pd.read_csv(CSV) 
+  df = weekly_update(city,'AQI')
   col='aqi'
   df = df[[col , 'dt']]
   df['dt'] =  pd.to_datetime(df['dt'], format='%Y%m%d %H:%M:%S')

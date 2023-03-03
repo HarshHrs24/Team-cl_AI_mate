5c9414888d15b04ea9f4466e1c9016d412f2b5fc
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-03-04 01:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -384,9 +384,9 @@ with st.container():
         
         # forecast['ds'] = pd.to_datetime(df['ds'])
         # st.write(forecast)
-        yhat=float(forecast.loc[forecast['ds']==d, 'yhat']).format("%2f")
-        yhat_upper=float(forecast.loc[forecast['ds']==d, 'yhat_upper'])
-        yhat_lower=float(forecast.loc[forecast['ds']==d, 'yhat_lower'])
+        yhat="{:.2f}".format(float(forecast.loc[forecast['ds']==d, 'yhat']))
+        yhat_upper="{:.2f}".format(float(forecast.loc[forecast['ds']==d, 'yhat_upper']))
+        yhat_lower="{:.2f}".format(float(forecast.loc[forecast['ds']==d, 'yhat_lower']))
         prediction_year_info="On {} the predicted temperature range for Adilabad is between {} and {}, with a most likely temperature of {}.".format(d,yhat_upper,yhat_lower,yhat)
         st.write(prediction_year_info)
 
